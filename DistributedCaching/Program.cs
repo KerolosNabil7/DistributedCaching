7ee469d306e41f1d53bfb51578fac92642157e2e
Kerolos Nabil <knabil@aknlab.com>
2025-12-11 17:02:26 +0200

    edit the program.cs

--- DistributedCaching/Program.cs
+++ DistributedCaching/Program.cs
@@ -1,3 +1,8 @@
+using DistributedCaching.Data;
+using DistributedCaching.DTOs;
+using DistributedCaching.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,19 +12,52 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//Register the SQLServer
+var DefaultConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(DefaultConnectionString));
+
+//Register Product service
+builder.Services.AddTransient<IProductService, ProductService>();
+
+//Register Distributed Cache - Redis
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = "localhost";
+    options.ConfigurationOptions = new StackExchange.Redis.ConfigurationOptions()
+    {
+        AbortOnConnectFail = true,
+        EndPoints = { options.Configuration }
+    };
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options=>options.DisplayRequestDuration());
 }
 
+app.MapGet("/products", async (IProductService service) =>
+{
+    var products = await service.GetAll();
+    return Results.Ok(products);
+});
+
+app.MapGet("/products/{id:guid}", async (Guid id, IProductService service) =>
+{
+    var product = await service.Get(id);
+    return Results.Ok(product);
+});
+
+app.MapPost("/products", async (ProductCreationDto product, IProductService service) =>
+{
+    await service.Add(product);
+    return Results.Created();
+});
+
 app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
 
 app.Run();
